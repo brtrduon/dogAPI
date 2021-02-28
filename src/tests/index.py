34727ef7198f769590a3e46a2f95a3f4fefc8f39
brtrduon <brianduongt@gmail.com>
2021-02-28 14:31:32 -0800

    added breed list test

--- src/tests/index.py
+++ src/tests/index.py
@@ -1,6 +1,27 @@
-from  selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-class clickTest():
-  def test(self):
+class dog_api_test():
+  breed_list = ["affenpinscher", "african", "airedale", "akita", "appenzeller", "australian-shepherd", "basenji", "beagle"]
+
+  def breed_search(self, breed_list=breed_list):
     baseUrl = "http://localhost:8080/"
     driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(baseUrl)
+    driver.implicitly_wait(10)
+
+    input_field = driver.find_element_by_class_name("form-control")
+
+    for breed in breed_list:
+      input_field.clear()
+      time.sleep(1)
+      input_field.send_keys(breed)
+
+      search_button = driver.find_element_by_class_name("btn")
+      search_button.click()
+      time.sleep(2)
+
+ff = dog_api_test()
+ff.breed_search()
