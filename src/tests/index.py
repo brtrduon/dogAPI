7ac78e7116ad8185e83436866c3e32b40c7a9ff5
brtrduon <brianduongt@gmail.com>
2021-03-02 15:04:35 -0800

    initialized reusable vars

--- src/tests/index.py
+++ src/tests/index.py
@@ -3,32 +3,33 @@ from selenium.webdriver.common.by import By
 import time
 
 class dog_api_test():
-  breed_list = ["affenpinscher", "corgi", "turkish angora", "asdf", "husky", "akita", "appenzeller", "basenji", "beagle"]
+  def __init__(self):
+    self.breed_list = ["affenpinscher", "corgi", "turkish angora", "asdf", "husky", "akita", "appenzeller", "basenji", "beagle"]
+    self.baseUrl = "http://localhost:8080/"
+    self.driver = webdriver.Chrome()  
 
-  def breed_search(self, breed_list=breed_list):
-    baseUrl = "http://localhost:8080/"
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get(baseUrl)
-    driver.implicitly_wait(10)
+  def breed_search(self):
+    self.driver.maximize_window()
+    self.driver.get(self.baseUrl)
+    self.driver.implicitly_wait(10)
 
-    input_field = driver.find_element_by_class_name("form-control")
+    input_field = self.driver.find_element_by_class_name("form-control")
 
-    for breed in breed_list:
+    for breed in self.breed_list:
       input_field.clear()
-
       input_field.send_keys(breed)
 
-      search_button = driver.find_element_by_class_name("btn")
-      search_button.click()
+      search_btn = self.driver.find_element_by_class_name("btn")
+      search_btn.click()
       time.sleep(1)
+      # sleep is needed to not leak "invalid breed" to valid breeds 
 
       try:
-        invalid_breed = driver.find_element_by_class_name("invalid-breed")
+        invalid_breed = self.driver.find_element_by_class_name("invalid-breed")
         print(breed.capitalize() + " is an invalid breed")
       except:
-        valid_breed = driver.find_element_by_class_name("dog-image")
-        print(breed.capitalize() + " is a valid breed")
+        valid_breed = self.driver.find_element_by_class_name("dog-image")
+        print(breed.capitalize() + " is a valid breed")    
 
 ff = dog_api_test()
 ff.breed_search()
